cb70fec95b0d071f0f2b3f027615242ba1662e5c
adzanfsw <193100062@almaata.ac.id>
2021-12-16 22:15:38 +0700

    create sidebar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
-import logo from './logo.svg'
-import './App.css'
-
-import { MdDeleteForever } from 'react-icons/md'
+import Sidebar from './components/UI/organisms/sidebar'
+// import { MdDeleteForever } from 'react-icons/md'
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl text-blue-600 font-bold underline">
-        Hello agents!
-      </h1>
-      <button
-        type="button"
-        className="bg-LightYellow2 rounded-full p-1"
-        aria-label="Delete"
-      >
-        <MdDeleteForever color="red" size="1.75rem" />
-      </button>
+        <Sidebar />
     </>
   )
 }
