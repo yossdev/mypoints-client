1bda4529a78e78fd67cbe967c7db921b1546ea38
yossdev <zenhanprogram@gmail.com>
2021-12-19 04:24:16 +0700

    update logo & icon

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import logo from './logo.svg'
 import './App.css'
 
-import { MdDeleteForever } from 'react-icons/md'
-
 function App() {
   return (
-    <>
-      <h1 className="text-3xl text-blue-600 font-bold underline">
-        Hello agents!
-      </h1>
-      <button
-        type="button"
-        className="bg-LightYellow2 rounded-full p-1"
-        aria-label="Delete"
-      >
-        <MdDeleteForever color="red" size="1.75rem" />
-      </button>
-    </>
+    <div>
+      <img src={logo} alt="logo" className="bg-violet-600" />
+      <h1 className="text-3xl font-bold underline">MyPoints Agent</h1>
+    </div>
   )
 }
 
