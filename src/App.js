29abc62c3c5d64f1485979eb7f9a41454183cbc3
adzanfsw <193100062@almaata.ac.id>
2021-12-19 22:14:48 +0700

    Update Route

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Redeems from "./components/pages/Redeems";
 import History from "./components/pages/History";
 import FAQ from "./components/pages/FAQ";
 import Profile from "./components/pages/Profile";
+import NotFound from "./components/pages/NotFound";
 
 function App() {
     return (
@@ -19,6 +20,8 @@ function App() {
                 <Route path="history" element={<History />} />
                 <Route path="faq" element={<FAQ />} />
                 <Route path="profile" element={<Profile />} />
+
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </>
     )
---
+++ src/components/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+
+
+function NotFound() {
+
+    return (
+        <>
+            <h1>Error 404</h1>
+        </>
+    );
+}
+
+export default NotFound;
