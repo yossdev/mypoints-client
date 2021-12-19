e408e1aba3943f5ccd7fac055cd05ef053652cbd
adzanfsw <193100062@almaata.ac.id>
2021-12-19 22:32:12 +0700

    Update index.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import HomePage from "./components/pages/HomePage";
 import Dashboard from "./components/pages/Dashboard";
@@ -12,17 +12,19 @@ import NotFound from "./components/pages/NotFound";
 function App() {
     return (
         <>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="dashboard" element={<Dashboard />} />
-                <Route path="claims" element={<Claims />} />
-                <Route path="redeems" element={<Redeems />} />
-                <Route path="history" element={<History />} />
-                <Route path="faq" element={<FAQ />} />
-                <Route path="profile" element={<Profile />} />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="dashboard" element={<Dashboard />} />
+                    <Route path="claims" element={<Claims />} />
+                    <Route path="redeems" element={<Redeems />} />
+                    <Route path="history" element={<History />} />
+                    <Route path="faq" element={<FAQ />} />
+                    <Route path="profile" element={<Profile />} />
 
-                <Route path="*" element={<NotFound />} />
-            </Routes>
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </BrowserRouter>
         </>
     )
 }
