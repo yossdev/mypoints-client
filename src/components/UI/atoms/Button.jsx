561e8c3fd4e4ab415e9ea9cad930fd7e0639b6b9
adzanfsw <193100062@almaata.ac.id>
2021-12-23 16:25:24 +0700

    banner component

---
+++ src/components/UI/atoms/Button.jsx
@@ -0,0 +1,9 @@
+const ButtonLogin = () => {
+  return (
+    <button className="mr-28 border-2 border-purple bg-white hover:bg-lightpurple text-purple text-sm font-roboto py-1.5 px-6 rounded-md">
+      Login
+    </button>
+  );
+};
+
+export default ButtonLogin;
